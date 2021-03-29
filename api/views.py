a62bae30f86c84ccd2ab7fc7f927fc10ed2ac049
Alduxsan <aldosantarelli@gmail.com>
2021-03-29 14:52:20 -0300

    funcionalidad para que cambie el estado de disponible de Guardia segun tenga o no un medico asignado

--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from Aplicaciones.Agenda.models import Guardia, Medico
@@ -8,7 +7,6 @@ from api.serializers import GuardiaSerializer
 from api import serializers
 from rest_framework.decorators import action
 from django.contrib.auth.models import User
-
 
 class GuardiasViewSet(viewsets.ModelViewSet):
 
